04cd76b10f481b5a185537e2819298b4c0337ec1
Karan Yuvraj Singh <56989043+karanysingh@users.noreply.github.com>
2020-02-19 21:25:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,22 +2,18 @@ from flask import Flask, render_template, Response
 from camera import VideoCamera
 
 app = Flask(__name__)
-
 @app.route('/')
-def index():
-    return render_template('index.html')
+def blabla():
+    while True:
+        frame,bla = VideoCamera().get_frame()
+        return render_template('index.html',bla=bla)
 
 def gen(camera):
     while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
+        frame,a= camera.get_frame()
+        yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+        
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen(VideoCamera()),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
-
-
+    return Response(gen(VideoCamera()),mimetype='multipart/x-mixed-replace; boundary=frame')
 app.run(debug=True)
-##flask app
