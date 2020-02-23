9481468da7a1beb2e70a59aac6ea9314d89b3d23
Karan Yuvraj Singh <56989043+karanysingh@users.noreply.github.com>
2020-02-23 12:47:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,51 @@
-from flask import Flask, render_template, Response, send_file
+from flask import Flask, render_template, Response,jsonify,send_file
 from camera import VideoCamera
-
 app = Flask(__name__)
+arr=[]
 @app.route('/')
-def status():
-    while True:
-        frame,ball = VideoCamera().get_frame()
-        return render_template('index.html',ball=ball)
+def index():
+    return render_template('index.html')
 
 def gen(camera):
+    global arr
     while True:
-        frame,a= camera.get_frame()
-        yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-        
-#sends videofeed
+        frame,a = camera.get_frame()
+        arr = arr[len(arr)-5:]
+        arr.append(a)
+        print(arr)
+        yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+#
+# def live(camera):
+#
+#     while True:
+#         frame,a=camera.get_frame()
+#         # yield(aa
+
+
 @app.route('/video_feed')
 def video_feed():
     return Response(gen(VideoCamera()),mimetype='multipart/x-mixed-replace; boundary=frame')
 
+@app.route('/params')
+def params():
+    ##return Response(live(VideoCamera),mimetype='text/event-stream')
+    # return jsonify(live(VideoCamera()
+    return jsonify({'arr':arr})
+
 #Passes image to html
 @app.route('/banner')
 def get_image():
-    filename = 'templates/Tech2.png'
+    filename = 'templates/technocrats.png'
     return send_file(filename, mimetype='image/gif')
+
+@app.route('/back')
+def get_back():
+    filename = 'templates/codewall.png'
+    return send_file(filename, mimetype='image/gif')
+
+@app.route('/font')
+def get_font():
+    filename = 'templates/OrionPax.ttf'
+    return send_file(filename, mimetype='font/ttf')
 
 app.run(debug=True)
